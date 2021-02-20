e179ba522841d2f2d26598d85881c56378759d0e
Sergey Makarenko <sergey.makarenko.web@gmail.com>
2021-02-20 12:38:02 +0200

    Add link to general google field

--- resources/js/components/general-depend-field/DetailField.vue
+++ resources/js/components/general-depend-field/DetailField.vue
@@ -1,5 +1,10 @@
 <template>
-    <panel-item :field="field" />
+  <panel-item :field="field">
+    <div slot="value">
+      <span v-if="!field.isLink">{{field.value}}</span>
+      <span v-else><a target="_blank" :href="field.value">{{field.value}}</a></span>
+    </div>
+  </panel-item>
 </template>
 
 <script>
